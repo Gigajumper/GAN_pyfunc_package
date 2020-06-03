5486556d9ed961a9dcbb5ce49877239a3b0d20e3
vik <47285016+Gigajumper@users.noreply.github.com>
2020-06-02 22:23:38 -0400

    flask application

---
+++ deploy_local2/app.py
@@ -0,0 +1,57 @@
+from flask import Flask, request, jsonify, send_file
+import mlflow.pyfunc
+#import pandas as pd
+import json
+from PIL import Image
+import io
+import numpy as np
+import torchvision.utils as vutils
+
+# Name of the apps module package
+app = Flask(__name__)
+
+# Load in the model at app startup
+model = mlflow.pyfunc.load_model('./model')
+
+# Load in our meta_data
+f = open("./model/code/meta_data.txt", "r")
+load_meta_data = json.loads(f.read())
+
+# Meta data endpoint
+@app.route('/', methods=['GET'])
+def meta_data():
+
+	return jsonify(load_meta_data)
+
+# Prediction endpoint
+@app.route('/predict', methods=['POST'])
+def predict():
+ req = request.get_json()
+	
+ fakeimage = model.predict(0)
+
+# print('\n\tfakeimg = ',type(fakeimage))
+# print('\n\tfakeimg.shape = ',fakeimage.shape)
+
+ imggrd = vutils.make_grid(fakeimage, padding=2, normalize=True)
+ imgnp = imggrd.numpy()
+ imgnpT = np.transpose(imgnp,(1,2,0))
+
+# print('\ntype(imgnp) =  ',type(imgnp))
+# print('\nshape(imgnp) = ',imgnp.shape) #(64, 3, 64, 64)
+
+ imgPIL = Image.fromarray((255*imgnpT).astype("uint8"),'RGB')
+
+ #create file-object in memory
+ file_object = io.BytesIO()
+
+# write PNG in file-object
+ imgPIL.save(file_object,'PNG')
+ 
+ file_object.seek(0)
+ 
+# Return prediction as response
+# return jsonify(['DONE : Ideally this should be an image'])
+ return send_file(file_object,mimetype='image/PNG')
+
+app.run(host='0.0.0.0', port=5000, debug=True)
